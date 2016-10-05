1b4d111353aa5d7e563aa49472af95df717efe9c
David-McNiven <davidmcniven@bell.net>
2016-10-05 17:51:00 -0400

    initialized form

--- Sharp Auto Center/Program.cs
+++ Sharp Auto Center/Program.cs
@@ -16,7 +16,7 @@ namespace Sharp_Auto_Center
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SharpAutoForm());
         }
     }
 }
--- Sharp Auto Center/SharpAuto.cs
+++ Sharp Auto Center/SharpAuto.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Sharp_Auto_Center
 {
-    public partial class Form1 : Form
+    public partial class SharpAutoForm : Form
     {
-        public Form1()
+        public SharpAutoForm()
         {
             InitializeComponent();
+        }
+
+        private void SharpAutoForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
